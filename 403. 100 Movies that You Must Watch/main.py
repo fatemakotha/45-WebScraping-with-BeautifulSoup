3e9e64e6384f71be62bffc1f7e45eb5181beb7e9
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-30 20:45:13 +0600

    Reverse order of list

---
+++ 403. 100 Movies that You Must Watch/main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+movies_page = requests.get(URL)
+movies_page = movies_page.text
+# print(movies_page)
+
+soup = BeautifulSoup(movies_page, "html.parser")
+# print(soup)
+
+movie_list = []
+
+titles = soup.find_all(name="h3", class_="title")
+print(titles)
+for each_item in titles:
+    mov_name_num = each_item.text
+    movie_list.append(mov_name_num)
+
+print(movie_list) #prints list from 100 to 0
+#Reversing the list starting from 1-100
+movie_list.reverse()
+print(movie_list)
+
